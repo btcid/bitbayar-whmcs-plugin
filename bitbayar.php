81f7b7f4f2e2b102bf34e58f71c871484a3a6e15
teddyfresnel <teddyfresnel@gmail.com>
2015-04-17 21:20:01 +0800

    update

--- bitbayar.php
+++ bitbayar.php
@@ -2,7 +2,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 BitBayar
+ * Copyright (c) 2014 Teddy Fresnel
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -23,6 +23,12 @@
  * THE SOFTWARE.
  */
 
+/**
+ * WHMCS Bitcoin payment module using Bitbayar system
+ * https://bitbayar.com
+ * @author Teddy Fresnel <teddyfresnel.com>
+ */
+
 function bitbayar_config()
 {
     $configarray = array(
@@ -30,57 +36,72 @@ function bitbayar_config()
             'Type' => 'System',
             'Value'=>'Bitcoin (BitBayar)'
         ),
-        'apiKey' => array(
-            'FriendlyName' => 'Token from your bitbayar.com merchant account.',
+        'apiToken' => array(
+            'FriendlyName' => 'API Token from your bitbayar.com merchant account.',
             'Type'         => 'text'
+        ),
+        'paymentInfo' => array(
+			'FriendlyName' => 'Payment Info',
+            'Type'         => 'textarea',
+            'Rows'         => '2',
+            'Description'  => 'payment information you want to be shown on the client invoice'
         )
     );
 
     return $configarray;
 }
 
-/**
- * Returns html form.
- *
- * @param  array  $params
- * @return string
- */
 function bitbayar_link($params)
 {
     if (false === isset($params) || true === empty($params)) {
         die('[ERROR] In modules/gateways/bitbayar.php::bitbayar_link() function: Missing or invalid $params data.');
     }
 
-    // Invoice Variables
+	//~ Bitbayar Variables
+	$api_token = $params['apiToken'];
+	$callback_url = $params['systemurl'] . "/modules/gateways/callback/bitbayar.php";
+	$redirect_url = $params['systemurl'] . "/viewinvoice.php?id=" . $params['invoiceid'];
+	
+    //~ Invoice Variables
     $invoiceid = $params['invoiceid'];
+	$description = $params["description"];
+	$amount = $params['amount']; # Format: ##.##
+	$currency = $params['currency']; # Currency Code
 
-    // Client Variables
-    //~ $firstname = $params['clientdetails']['firstname'];
-    //~ $lastname  = $params['clientdetails']['lastname'];
-    //~ $email     = $params['clientdetails']['email'];
-    //~ $address1  = $params['clientdetails']['address1'];
-    //~ $address2  = $params['clientdetails']['address2'];
-    //~ $city      = $params['clientdetails']['city'];
-    //~ $state     = $params['clientdetails']['state'];
-    //~ $postcode  = $params['clientdetails']['postcode'];
-    //~ $country   = $params['clientdetails']['country'];
-    //~ $phone     = $params['clientdetails']['phonenumber'];
+    //~ Client Variables
+    $firstname = $params['clientdetails']['firstname'];
+    $lastname  = $params['clientdetails']['lastname'];
+    $email     = $params['clientdetails']['email'];
+    $address1  = $params['clientdetails']['address1'];
+    $address2  = $params['clientdetails']['address2'];
+    $city      = $params['clientdetails']['city'];
+    $state     = $params['clientdetails']['state'];
+    $postcode  = $params['clientdetails']['postcode'];
+    $country   = $params['clientdetails']['country'];
+    $phone     = $params['clientdetails']['phonenumber'];
 
-    // System Variables
-    $systemurl = $params['systemurl'];
+    //~ System Variables    
+	$systemurl = $params['systemurl'];
+	$companyname = $params['companyname'];
+	$currency = $params['currency'];
 
     $post = array(
-        'invoice_id'     => $invoiceid,
-        'systemURL'     => $systemurl
+		'token'			=>$api_token,
+		'invoice_id'	=>$invoiceid,
+		'rupiah'		=>$amount,
+		'memo'			=>$description,
+		'callback_url'	=>$callback_url,
+		'url_success'	=>$redirect_url,
+		'url_failed'	=>$redirect_url
     );
 
-    $form = '<form action="' . $systemurl . '/modules/gateways/bit-bayar/createinvoice.php" method="POST">';
+    $form = '<form action="' . $systemurl . '/modules/gateways/bitbayar/create_invoice.php" method="POST">';
 
     foreach ($post as $key => $value) {
         $form .= '<input type="hidden" name="' . $key . '" value = "' . $value . '" />';
     }
 
-    $form .= '<input type="submit" value="' . $params['langpaynow'] . '" />';
+    $form .= '<br/><button type="submit" style="border:none;padding:0;background:0;"><img src="https://bitbayar.com/images/button/buy-white-small.png"/></button><br/>'.$params['paymentInfo'];
     $form .= '</form>';
 
     return $form;
--- bitbayar/create_invoice.php
+++ bitbayar/create_invoice.php
@@ -28,7 +28,7 @@
  * https://bitbayar.com
  * @author Teddy Fresnel <teddyfresnel.com>
  */
- 
+
 //~ Required File 
 include '../../../dbconnect.php';
 include '../../../includes/functions.php';
@@ -36,43 +36,42 @@ include '../../../includes/gatewayfunctions.php';
 include '../../../includes/invoicefunctions.php';
 
 $gatewaymodule = 'bitbayar';
-$GATEWAY       = getGatewayVariables($gatewaymodule);
+$GATEWAY = getGatewayVariables($gatewaymodule);
 
-if (!$GATEWAY['type']) {
-	logTransaction($GATEWAY['name'], $_POST, 'Not activated');
-	bbLog('[ERROR] In modules/gateways/callback/bitbayar.php: BitBayar module not activated');
-	die('[ERROR] In modules/gateways/callback/bitbayar.php: BitBayar module not activated.');
-}
 
-//~ Checks invoice ID is a valid invoice number or ends processing
-$invoiceid = $_POST['invoice_id'];
-$invoiceid = checkCbInvoiceID($invoiceid, $GATEWAY['name']);
-
-//~ Required File Checks transaction number isn't already in the database and ends processing if it does
-$transid = $_POST['id'];        
-checkCbTransID($transid);
-
-//~ Get invoice
-$result    = mysql_query("SELECT id, total from tblinvoices where id=".mysql_real_escape_string($invoiceid));
+//~ get invoice
+$invoiceId = (int) $_POST['invoice_id'];
+$price     = $currency = false;
+$result    = mysql_query("SELECT tblinvoices.total, tblinvoices.status, tblcurrencies.code FROM tblinvoices, tblclients, tblcurrencies where tblinvoices.userid = tblclients.id and tblclients.currency = tblcurrencies.id and tblinvoices.id=$invoiceId");
 $data_respon      = mysql_fetch_assoc($result);
 
-//~ Check IDR value
-if($_POST['rp']!=(int) $data_respon['total']){
-	logTransaction($GATEWAY['name'], $_POST, 'Jumlah Rupiah yang dikirim tidak benar: '.$_POST['rp'].'!='.(int)$data_respon['total'] );
-	return;
+if (!$data_respon) {
+    bbLog('[ERROR] In modules/gateways/bitbayar/createinvoice.php: No invoice found for invoice id #' . $invoiceId);
+    die('[ERROR] In modules/gateways/bitbayar/createinvoice.php: Invalid invoice id #' . $invoiceId);
 }
 
-//~ Check double spend
-if($_POST['auto_sell']!=1){			
-	logTransaction($GATEWAY['name'], $_POST, 'Double Transaksi.');
-	return;
+$price    = $data_respon['total'];
+$currency = $data_respon['code'];
+$status   = $data_respon['status'];
+
+if ($status != 'Unpaid') {
+    bbLog('[ERROR] In modules/gateways/bitbayar/createinvoice.php: Invoice status must be Unpaid.  Status: ' . $status);
+    die('[ERROR] In modules/gateways/bitbayar/createinvoice.php: Bad invoice status of ' . $status);
 }
 
-//~ Double check to https://bitbayar.com/api/check_invoice
-$data['token']	= $GATEWAY['apiToken'];
-$data['id']      = $transid;
 
-$url = 'https://bitbayar.com/api/check_invoice';
+//~ Create invoice
+$data = array(
+    'token'			=>$GATEWAY['apiToken'],
+    'invoice_id'	=>$invoiceId,
+    'rupiah'		=>$data_respon['total'],
+    'memo'			=>$_POST['memo'],
+    'callback_url'	=>$_POST['callback_url'],
+    'url_success'	=>$_POST['url_success'],
+    'url_failed'	=>$_POST['url_failed']
+);
+
+$url = 'https://bitbayar.com/api/create_invoice';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -83,18 +82,12 @@ $return = curl_exec($ch);
 curl_close($ch);
 $response = json_decode($return);
 
-//~ Check if status has paid and ends processing if it does
-if($response->success!=1 && $response->status!='paid'){
-	logTransaction($GATEWAY['name'], $_POST, 'Pembayaran Belum Lengkap.');
-	return;
+
+if($response->success){
+	header('Location: '.$response->payment_url);
+	exit;
 }
-
-//~ Successful
-$fee = 0;
-
-//~ Left blank, this will auto-fill as the full balance
-$amount = '';
-
-//~ Add transaction payment
-addInvoicePayment($invoiceid, $transid, $amount, $fee, $gatewaymodule);
-logTransaction($GATEWAY['name'], $_POST, 'Pembayaran Sukses.');
+else{
+	bbLog('[ERROR] In modules/gateways/bitbayar/create_invoice.php: Invoice error: ' . var_export($response['error'], true));
+    die('[ERROR] In modules/gateways/bitbayar/create_invoice.php: Invoice error: ' . var_export($response['error']['error_message'], true));
+}
